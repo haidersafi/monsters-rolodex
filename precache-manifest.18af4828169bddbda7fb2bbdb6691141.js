1e159b516ff0d2dc0d81ba31908439679f53988a
Safi Saqlain <safisaqlain@gmail.com>
2019-12-23 01:58:23 +0500

    Updates

---
+++ precache-manifest.18af4828169bddbda7fb2bbdb6691141.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797f4244ae9ceced562239f7ee595dbc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d830f73e842697dbbb21",
+    "url": "/monsters-rolodex/static/css/main.53cc3bbd.chunk.css"
+  },
+  {
+    "revision": "a8b76958ccd5d6d253a3",
+    "url": "/monsters-rolodex/static/js/2.7029a57e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7029a57e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d830f73e842697dbbb21",
+    "url": "/monsters-rolodex/static/js/main.59355088.chunk.js"
+  },
+  {
+    "revision": "a565b40e5380e6418ba7",
+    "url": "/monsters-rolodex/static/js/runtime-main.bf85e43f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
